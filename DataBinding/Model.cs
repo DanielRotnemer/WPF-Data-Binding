a8cc735f48594bc54b630571da1e60efc7110048
DanielRotnemer <traderrt955@gmail.com>
2023-02-09 10:03:59 +0200

    first commit

---
+++ DataBinding/Model.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    public class Model : ViewModelBase
+    {
+        private int number = 555;
+        private string text = "abcdef";
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+                OnPropertyChanged(nameof(Number));
+            }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+    }
+}
---
+++ DataBinding/Viewer.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    public partial class Viewer : Window
+    {
+        private static Model model = new Model { Number = 1234, Text = "ABCDEF" };
+
+        public Viewer()
+        {
+            InitializeComponent();
+            DataContext = model;
+        }
+
+        public static Model Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+    }
+}
